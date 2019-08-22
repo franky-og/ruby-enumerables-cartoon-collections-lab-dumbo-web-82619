c65bfe9bb412391fcc6dd01ac5f65d47eb1ff470
Franky Liu <cuifranky95@gmail.com>
2019-08-22 20:56:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.map {|index| index.length > 4? }
+  array.map {|index| index.length > 4? return true: }
 end
 
 def find_the_cheese# code an argument here

81e46410f27e278553157c043771246548c98955
Franky Liu <cuifranky95@gmail.com>
2019-08-22 21:20:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,5 +28,9 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types)
+  if array.include?(cheese_types) 
+    puts cheese_types
+    break
+  else
+    return nil
 end

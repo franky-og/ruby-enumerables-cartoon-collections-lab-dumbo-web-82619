4ca2a2e1a3b1c6444fa7137ab6071efefe455e40
Franky Liu <cuifranky95@gmail.com>
2019-08-22 21:25:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,7 +31,7 @@ def find_the_cheese(array)# code an argument here
   array.map {|index| 
   if array.include?(cheese_types) == true
     return cheese_types
-    
+    break
   else
     return nil
   end

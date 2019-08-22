923705366e39857642895e9272dd65db8f03edac
Franky Liu <cuifranky95@gmail.com>
2019-08-22 21:26:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,7 @@ def find_the_cheese(array)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.map {|index| 
   if array.include?(cheese_types) == true
-    return cheese_types
+    return index
     break
   else
     return nil
